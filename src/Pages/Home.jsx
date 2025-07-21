47ee86cc74852e0322bd2f1182044f16e5ecb646
PjrCodes <25995849+PjrCodes@users.noreply.github.com>
2025-07-21 15:16:34 +0530

    finished home page changes - no motion

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -28,7 +28,7 @@ const marqueeImages = [
 export default function Home() {
   const isMobile = useIsMobile();
   return (
-    <div className="w-full flex flex-col items-center mt-32">
+    <main className="w-full flex flex-col items-center mt-32">
       <div>
         <h1 className="text-6xl md:text-9xl font-urbanist text-white/80 font-normal leading-none mb-20">
           Intern <br></br>Portal
@@ -70,7 +70,7 @@ export default function Home() {
         <span className="text-[#ff0000]">Adobe's</span> Core Values
       </div>
       {isMobile ? (
-        <div className="grid grid-cols-2 grid-rows-2 gap-0 w-[90vw] max-w-[400px] mx-auto mt-8 overflow-hidden rounded-3xl">
+        <div className="grid grid-cols-2 grid-rows-2 gap-0 w-[90vw] max-w-[400px] mx-auto mt-8 overflow-hidden rounded-3xl mb-8">
           {/* 1 */}
           <div className="flex flex-col justify-center items-center bg-[#FFF7E1] text-[#ff0000] px-4 py-8 rounded-tl-3xl">
             <div className="text-5xl font-urbanist text-center">1</div>
@@ -101,33 +101,33 @@ export default function Home() {
           </div>
         </div>
       ) : (
-        <div className="relative w-full mx-auto mt-8 flex justify-center items-center">
+        <div className="relative w-full mx-auto mt-8 flex justify-center items-center mb-8">
           <div
             className={`h-full flex flex-row justify-center items-stretch rounded-3xl bg-[#FFF7E1] text-[#ff0000]`}
           >
-            <div className="min-h-full px-8 py-6 lg:px-16 lg:py-12 text-center flex-col flex justify-center items-center w-40 lg:w-64">
+            <div className="min-h-full px-8 py-6 lg:px-16 lg:py-12 text-center flex flex-col justify-center items-center w-40 lg:w-64">
               <div className="text-9xl font-urbanist text-center">1</div>
-              <div className="text-md font-medium text-center font-bold font-urbanist">
+              <div className="text-sm font-medium text-center font-bold font-urbanist">
                 Create the future.
               </div>
             </div>
             <div className="min-h-full text-center flex-row flex justify-center items-stretch bg-[#FF0000]/50 text-[#FFF7E1] rounded-3xl">
-              <div className="min-h-full px-8 py-6 lg:px-16 lg:py-12 text-center flex-col flex justify-center items-center w-40 lg:w-64">
+              <div className="min-h-full px-8 py-6 lg:px-16 lg:py-12 text-center flex flex-col justify-center items-center w-40 lg:w-64">
                 <div className="text-9xl font-urbanist text-center">2</div>
-                <div className="text-md font-medium text-center font-bold font-urbanist">
+                <div className="text-sm font-medium text-center font-bold font-urbanist">
                   Own the outcome.
                 </div>
               </div>
               <div className="min-h-full text-center flex-row flex justify-center items-stretch bg-[#ff0000] text-[#FFF7E1] rounded-3xl">
-                <div className="min-h-full px-8 py-6 lg:px-16 lg:py-12 text-center flex-col flex justify-center items-center w-40 lg:w-64">
+                <div className="min-h-full px-8 py-6 lg:px-16 lg:py-12 text-center flex flex-col justify-center items-center w-40 lg:w-64">
                   <div className="text-9xl font-urbanist text-center">3</div>
-                  <div className="text-md font-medium text-center font-bold font-urbanist">
+                  <div className="text-sm font-medium text-center font-bold font-urbanist">
                     Raise the bar.
                   </div>
                 </div>
-                <div className="min-h-full px-8 py-6 lg:px-16 lg:py-12 text-center flex-col flex justify-center items-center bg-[#cc0000] text-[#FFF7E1] rounded-3xl w-40 lg:w-64">
+                <div className="min-h-full px-8 py-6 lg:px-16 lg:py-12 text-center flex flex-col justify-center items-center bg-[#cc0000] text-[#FFF7E1] rounded-3xl w-40 lg:w-64">
                   <div className="text-9xl font-urbanist text-center">4</div>
-                  <div className="text-md font-medium text-center font-bold font-urbanist">
+                  <div className="text-sm font-medium text-center font-bold font-urbanist">
                     Be genuine.
                   </div>
                 </div>
@@ -137,11 +137,11 @@ export default function Home() {
         </div>
       )}
 
-      <div className="mt-14 text-[#FFF7E1] font-urbanist text-[20px] md:text-5xl ">
+      <div className="text-[#FFF7E1] font-urbanist text-[20px] md:text-5xl mb-8">
         Our team - ACS!
       </div>
 
-      <p className=" text-[14px] md:text-[16px] mt-20  text-center font-instrument text-[#FFF7E1] max-w-[840px] mb-8">
+      <p className=" text-[14px] md:text-[16px] text-center font-instrument text-[#FFF7E1] max-w-[840px] mb-8">
         This internship portal contains information about all the projects that
         interns have worked on this summer, the events like Adobe Alchemy,
         connecting with leaders, and much more. Get a glimpse into intern life
@@ -150,19 +150,25 @@ export default function Home() {
 
       {/* flow chart here */}
 
-      <p className=" text-[14px] md:text-[16px] mt-20  text-center font-instrument text-[#FFF7E1] max-w-[840px] mb-8">
+      <p className=" text-[14px] md:text-[16px] mb-8 text-center font-instrument text-[#FFF7E1] max-w-[840px] mb-8">
         Read more about the team, or see more pictures, by clicking the buttons
         below!
       </p>
 
       <div className="flex gap-4 bg-black p-8 justify-center">
-        <button className="w-[164px] h-[55px] rounded-[32px] bg-red-600 text-white font-medium hover:bg-red-700 transition">
+        <a
+          href="/teams"
+          className="flex flex-row items-center justify-center w-[164px] h-[55px] rounded-[32px] bg-red-600 text-white font-medium hover:bg-red-700 transition"
+        >
           Find Out More!
-        </button>
-        <button className="w-[164px] h-[55px] rounded-[32px] border-2 border-red-600 text-white font-medium hover:bg-red-600 transition">
+        </a>
+        <a
+          href="/gallery"
+          className="flex flex-row items-center justify-center w-[164px] h-[55px] rounded-[32px] border-2 border-red-600 text-white font-medium hover:bg-red-600 transition"
+        >
           View Gallery
-        </button>
+        </a>
       </div>
-    </div>
+    </main>
   );
 }
