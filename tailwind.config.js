4c555b9dc6231adbb2a5dd393a969688544254ce
PjrCodes <25995849+PjrCodes@users.noreply.github.com>
2025-07-21 13:22:59 +0530

    remove flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-import flowbiteReact from "flowbite-react/plugin/tailwindcss";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    ".flowbite-react\\class-list.json"
   ],
   theme: {
     extend: {
@@ -15,5 +12,5 @@ export default {
       },
     },
   },
-  plugins: [flowbiteReact],
+  plugins: [],
 };
